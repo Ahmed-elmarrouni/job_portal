ecefde407ee3b38029838143ef35c4883ca3eaeb
Ahmed El marrouni <ahmedelmarrouni1@gmail.com>
2024-05-29 12:47:51 +0100

    Add new seeders

--- database/seeders/OrdersTableSeeder.php
+++ database/seeders/OrdersTableSeeder.php
@@ -1,17 +1,28 @@
 <?php
 
-namespace Database\Seeders;
-
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 
 class OrdersTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        $faker = Faker::create();
+
+        for ($i = 0; $i < 10; $i++) {
+            DB::table('orders')->insert([
+                'company_id' => $faker->numberBetween(1, 10),
+                'package_id' => $faker->numberBetween(1, 10),
+                'order_no' => $faker->uuid,
+                'paid_amount' => $faker->randomFloat(2, 100, 1000),
+                'payment_method' => $faker->randomElement(['Credit Card', 'PayPal', 'Bank Transfer']),
+                'start_date' => $faker->date,
+                'expire_date' => $faker->date,
+                'currently_active' => $faker->boolean,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 }
